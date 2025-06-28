3e0edde1d3cf00ce769c7d4d7a699eb2ff69bc76
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-06-28 10:06:40 -0400

    update

--- crypto-gui-ae8bd7731ab8e22a.js
+++ crypto-gui-ae8bd7731ab8e22a.js
@@ -1560,20 +1560,20 @@ function __wbg_get_imports() {
         const ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper8230 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 3779, __wbg_adapter_32);
+    imports.wbg.__wbindgen_closure_wrapper8194 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 3509, __wbg_adapter_32);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper8232 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 3779, __wbg_adapter_35);
+    imports.wbg.__wbindgen_closure_wrapper8196 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 3509, __wbg_adapter_35);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper8234 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 3779, __wbg_adapter_32);
+    imports.wbg.__wbindgen_closure_wrapper8198 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 3509, __wbg_adapter_32);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper8328 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 3821, __wbg_adapter_40);
+    imports.wbg.__wbindgen_closure_wrapper8292 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 3551, __wbg_adapter_40);
         return addHeapObject(ret);
     };
 
@@ -1627,7 +1627,7 @@ async function __wbg_init(module_or_path) {
     console.warn('using deprecated parameters for the initialization function; pass a single object instead')
 
     if (typeof module_or_path === 'undefined') {
-        module_or_path = new URL('crypto-gui-5fcc6476a773d493_bg.wasm', import.meta.url);
+        module_or_path = new URL('crypto-gui-ae8bd7731ab8e22a_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
